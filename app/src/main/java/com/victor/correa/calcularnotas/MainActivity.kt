1e6246705d404bf0181f49d64c26b6a9cd0779ff
victornatera <160202495+victornatera@users.noreply.github.com>
2024-02-21 09:38:02 -0500

    validar porcentaje y validar notas agregar funcion validarNota agregarfuncion validarPocentaje agregar funcion actulizarProgress se creo una condicion para añadir porcentaje en la lista de porcentaje, se pone un text para que se reinicie y empiece de cero para poner los porcentajes y notas

--- app/src/main/java/com/victor/correa/calcularnotas/MainActivity.kt
+++ app/src/main/java/com/victor/correa/calcularnotas/MainActivity.kt
@@ -11,12 +11,14 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var progress : ProgressBar
     private lateinit var nombreUsuario : EditText
-    private lateinit var porcentaje : EditText
-    private lateinit var notas : EditText
+    private lateinit var ingresarPorcentaje : EditText
+    private lateinit var ingresarNotas : EditText
     private lateinit var finalizar : Button
-    private lateinit var enviar : Button
+    private lateinit var guardar : Button
 
     private var porcentajeAcumulado = 0
+    val listaNotas : MutableList<Double> = mutableListOf()
+    val listaPorcentaje : MutableList<Int> = mutableListOf()
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,16 +27,37 @@ class MainActivity : AppCompatActivity() {
 
 
         progress = findViewById(R.id.progress)
-        nombreUsuario = findViewById(R.id.nombreUsuario)
-        porcentaje = findViewById(R.id.porcentaje)
-        notas = findViewById(R.id.notas)
+        nombreUsuario = findViewById(R.id.ingresarNombre)
+        ingresarPorcentaje = findViewById(R.id.ingresarPorcentaje)
+        ingresarNotas = findViewById(R.id.ingresarNotas)
         finalizar = findViewById(R.id.finalizar)
-        enviar = findViewById(R.id.enviar)
+        guardar = findViewById(R.id.guardar)
 
+        guardar.setOnClickListener {
+
+            val nota = (ingresarNotas.text.toString()).toDouble()
+            val porcentaje = (ingresarPorcentaje.text.toString()).toInt()
+            if (validarNota(nota) && validarPorcentaje(porcentaje)){
+                listaNotas.add(nota)
+                listaPorcentaje.add(porcentaje)
+                porcentajeAcumulado += porcentaje
+
+                ingresarNotas.text.clear()
+                ingresarPorcentaje.text.clear()
+
+            }else{
+                //TODO informar al usuario que la nota ingresada no es valida
+            }
+        }
     }
-    fun validarPorcentaje (porcentaje : Int ){
-
+    fun actualizarProgreso(porcentaje : Int) {
+        progress.progress = porcentaje
     }
-
+    fun validarNota(nota : Double) : Boolean{
+        return nota >= 0 && nota <= 5
+    }
+    fun validarPorcentaje(porcentaje : Int ) : Boolean{
+        return porcentajeAcumulado + porcentaje <=100
+    }
 
 }
